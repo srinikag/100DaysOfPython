bc55230e4bcdda5ee424717a105a2cbcf2042561
Srinika <srinikagonugunta@gmail.com>
2024-08-11 20:46:52 -0500

    Day-5

---
+++ 05-multiplication_table.py
@@ -0,0 +1,6 @@
+num = 1
+user_input = int(input("What number do you want to multiply?"))
+while num <= 12:
+    answer = user_input * num
+    print(f" {user_input} x {num} = {answer}")
+    num += 1
